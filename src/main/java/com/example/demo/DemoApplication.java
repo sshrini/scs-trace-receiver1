cc336f18f74a6ced90efe6027084552527b12997
Sudeep Shrinivasan <sudeepshrinivasan@Sudeeps-MacBook-Pro.local>
2020-04-12 16:28:20 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.integration.annotation.ServiceActivator;
+
+
+
+
+@SpringBootApplication
+public class DemoApplication {
+	@Autowired
+	private MessageSenderSvc.PubsubOutboundGateway messagingGateway;
+
+	private static final Log LOGGER = LogFactory.getLog(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public MessageChannel pubsubInputChannel() {
+		return new DirectChannel();
+	}
+
+
+	@Bean
+	public PubSubInboundChannelAdapter messageChannelAdapter(
+			@Qualifier("pubsubInputChannel") MessageChannel inputChannel,
+			PubSubTemplate pubSubTemplate) {
+		PubSubInboundChannelAdapter adapter =
+				new PubSubInboundChannelAdapter(pubSubTemplate, "newSubscription");
+		adapter.setOutputChannel(inputChannel);
+
+		return adapter;
+	}
+
+	@ServiceActivator(inputChannel = "pubsubInputChannel")
+	public void messageReceiver(String payload) {
+
+		this.messagingGateway.sendToPubsub(payload);
+
+	}
+}
